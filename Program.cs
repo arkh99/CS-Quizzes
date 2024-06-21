dc2885a42e23b428c2d3bdd99eb201d86b396d57
Alireza Khoshnami <124736009+arkh99@users.noreply.github.com>
2024-06-20 22:55:44 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Quizzes;
+
+internal class Program
+{
+  private static Quiz quiz = new Quiz(Helper.GetAvailableQuestions());
+  enum Action { 
+    Add = 1, 
+    Play, 
+    Exit
+  }
+  private static void Main(string[] args)
+  {
+    bool isActive = true;
+
+    while (isActive)
+    {
+      Console.WriteLine("\nQuiz Application");
+      Console.WriteLine("1. Add Question");
+      Console.WriteLine("2. Play Quiz");
+      Console.WriteLine("3. Exit");
+      Console.Write("Enter an action: ");
+      
+      bool isValid = int.TryParse(Console.ReadLine(), out int action);
+
+      switch ((Action)action)
+      {
+        case Action.Add:
+          Add();
+          break;
+        case Action.Play:
+          Play();
+          break;
+        case Action.Exit:
+          isActive = false;
+          break;
+        default:
+          Console.WriteLine("Not a valid response.");
+          break;
+      }
+    }
+  }
+
+  private static void Add ()
+  {
+    string? text = null;
+    string? answer = null;  
+    
+    while (string.IsNullOrWhiteSpace(text)) 
+    {
+      Console.Write("Enter the question: ");
+      text = Console.ReadLine();
+    }
+
+    while (string.IsNullOrWhiteSpace(answer))
+    {
+      Console.Write("Enter the answer: ");
+      answer = Console.ReadLine();
+    }
+
+    try 
+    {
+      quiz.AddQuestion(text, answer);
+    } 
+    catch (Exception e) 
+    {
+      Console.WriteLine(e.Message);
+    }
+  }
+
+  private static void Play () {
+    try 
+    {
+      quiz.Play();
+    }
+    catch (Exception e) 
+    {
+      Console.WriteLine(e.Message);
+    }
+  }
+}
